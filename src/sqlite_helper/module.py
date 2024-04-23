d1f485017b20cdf380610c850bd3e9d89f03e771
Flo <93609174+Xeferis@users.noreply.github.com>
2024-04-23 14:29:45 +0000

    Fixed saving

--- src/sqlite_helper/module.py
+++ src/sqlite_helper/module.py
@@ -1,29 +1,29 @@
 import sqlite3
 
 @staticmethod
-def __check_db(name: str, path: str) -> bool:
+def __check_db(name: str) -> bool:
     exst = False
     return exst
 
 class generator():
 
-    def __init__(self, name: str, path: str) -> None:
-        if __check_db:
+    def __init__(self, db_name: str) -> None:
+        if __check_db(db_name):
             pass
         else:
-            self.__db(name, path)
+            self.__gen_db()
     
-    def __db(self, name: str, path: str) -> bool:
+    def __gen_db(self) -> bool:
         scss = False
         return scss
     
-    def add_table(self, name: str, col: dict) -> bool:
+    def add_table(self, tbl_name: str, col: dict) -> bool:
         scss = False
         return scss
 
 
 class establish():
 
-    def __init__(self) -> None:
-        if not __check_db:
-            raise ConnectionError("Database does not exist. Please check the path or generate a database first!")
+    def __init__(self, db_name: str) -> None:
+        if not __check_db(db_name):
+            raise ConnectionError("Database does not exist. Check name or generate a database first!")
