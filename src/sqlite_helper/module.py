2289f94116847706c85eed424580fb079dd3ebd1
Flo <93609174+Xeferis@users.noreply.github.com>
2024-04-23 14:00:16 +0000

    Basic Structure

---
+++ src/sqlite_helper/module.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+class generator():
+
+    def __init__(self) -> None:
+        pass
+
+    def table(self, name: str, col: dict) -> str:
+        pass
+        return ""
+
+
+class load():
+
+    def __init__(self) -> None:
+        pass
