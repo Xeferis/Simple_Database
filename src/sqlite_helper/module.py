e1cb39b6882f36779dc5523d8f0caceea22a0d8d
Flo <93609174+Xeferis@users.noreply.github.com>
2024-04-23 14:19:44 +0000

    Added existens checks

--- src/sqlite_helper/module.py
+++ src/sqlite_helper/module.py
@@ -1,16 +1,29 @@
 import sqlite3
 
+@staticmethod
+def __check_db(name: str, path: str) -> bool:
+    exst = False
+    return exst
+
 class generator():
 
+    def __init__(self, name: str, path: str) -> None:
+        if __check_db:
+            pass
+        else:
+            self.__db(name, path)
+    
+    def __db(self, name: str, path: str) -> bool:
+        scss = False
+        return scss
+    
+    def add_table(self, name: str, col: dict) -> bool:
+        scss = False
+        return scss
+
+
+class establish():
+
     def __init__(self) -> None:
-        pass
-
-    def table(self, name: str, col: dict) -> str:
-        pass
-        return ""
-
-
-class load():
-
-    def __init__(self) -> None:
-        pass
+        if not __check_db:
+            raise ConnectionError("Database does not exist. Please check the path or generate a database first!")
